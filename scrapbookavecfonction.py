5c5bb9f285fc452919d40d39b2a6b8ffcbc58744
zhao yh <makai87kai@outlook.com>
2023-02-07 22:47:47 +0100

    finir les projet avec les fonction,fonction de prendre tous les liens des category,fonction de prendre tous les lien dans une category, fonction chercher bouton <next page> dans la page,fonction prendres tous les liens de chaque produit,fonction de prendre tous les informations demande dans une page

--- scrapbookavecfonction.py
+++ scrapbookavecfonction.py
@@ -1,68 +1,117 @@
-from requests_html import HTMLSession
 import csv
 import os
+from requests_html import HTMLSession
 
-def url_de_page_suivante(url_actuelle) :
-    session = HTMLSession()
-    page = session.get(url_actuelle)
-    if page.html.find('li.next a', first='True') != None :
-        url_actuelle = list(page.html.find('li.next a', first='True').absolute_links)[0]
-        return url_actuelle
-    else :
-        return None
 
-def element_page(url) :
-    session = HTMLSession()
+# find if there is a next button in the page
+def next_page_url(page):
+    next_page_link_a_tag = page.html.find('li.next a', first='True')
+    if next_page_link_a_tag is not None:
+        next_page_link = list(next_page_link_a_tag.absolute_links)
+        return next_page_link[0]
+    else:
+        nex_page_link = None
+        return nex_page_link
+
+
+# find all the category links in the first page except category:"book"
+def get_links_of_category_in_first_page(first_page_url):
+    page = session.get(first_page_url)
+    category_links = []
+    all_category_content = page.html.find('ul.nav-list a')
+    for category_content in all_category_content:
+        category_links = category_links + list(category_content.absolute_links)
+    category_links.pop(0)
+    return category_links
+
+
+# get all the links in a single detail page of category
+def get_links_of_books_in_page(page):
+    # For Tuple, List, Optional objects, look at the module 'typings' in the standard library
+
+    # Find book links in the page
+    books_a_tags = page.html.find('div.image_container a')
+    book_links_in_page = []
+
+    for a_tag in books_a_tags:
+        book_links_in_page = book_links_in_page + list(a_tag.absolute_links)
+
+    return book_links_in_page
+
+
+# get all the book's links of the site
+def get_all_books_links_of_one_category(category_link):
+    all_books_links = []
+    book_links = []
+    next_page_link = category_link
+    while next_page_link is not None:
+        page = session.get(next_page_link)
+        book_links = get_links_of_books_in_page(page)
+        next_page_link = next_page_url(page)
+        all_books_links = all_books_links + book_links
+
+    return all_books_links
+
+
+# get diver information from a single book in detail page
+def get_data_in_book_page(url):
     page = session.get(url)
-    titre = page.html.find('div.product_main h1', first='true').text
+    download_links_of_images = []
+    title = page.html.find('div.product_main h1', first='true').text
     category = page.html.find('.breadcrumb > li:nth-child(3) > a:nth-child(1)')[0].text
-    production_descripton = page.html.find('article.product_page p', first='true').text
-    liens = page.html.absolute_links
+    production_descripton_p_tag = page.html.find('article.product_page p')
+    production_descripton = production_descripton_p_tag[3].text
     code_upc = page.html.find('table.table-striped tr td', first='true').text
-    # print(f'the code upc of this book is :{code_upc}')
-    star_rating = page.html.find('p.star-rating', first='true')
-    star = star_rating.attrs.get('class')[1]
-    price_withouttaxe = page.html.find('table.table-striped tr td')[2].text
-    price = page.html.find('table.table tr td')[3].text
-    stock = page.html.find('table.table tr td')[5].text
-    image_url = str(page.html.find('div.active img')[0].attrs.get('src'))
-    return [titre],[category],[production_descripton],[code_upc],[star],[price_withouttaxe],[price],[stock,image_url]
+    star_rating_information = page.html.find('p.star-rating', first='true')
 
-def url_des_page_category(url) :
-    session = HTMLSession()
-    page = session.get(url)
-    urls = []
-    tous_les_liens_par_category = page.html.find('ul.nav-list a')
-    for lien in tous_les_liens_par_category :
-        urls.append(list(lien.absolute_links))
-    del(urls[0])
-    return urls
+    # using the arguments attrs to get the star rating informations
+    star_rating = star_rating_information.attrs.get('class')[1]
 
-def urls_dune_category(url) :
-    session = HTMLSession()
-    urlspage = []
-    i = 0
-    while url != None :
-        page = session.get(url)
-        while i < len(page.html.find('div.image_container a')) :
-            urlspage.append = list(page.html.find('div.image_container a')[i].absolute_links)
-            i += 1
-        url = url_de_page_suivante(url)
+    price_without_tax = page.html.find('table.table-striped tr td')[2].text
+    price_with_tax = page.html.find('table.table tr td')[3].text
+    inventory = page.html.find('table.table tr td')[5].text
 
-    return urlspage
+    # image links to absolute links by re write the link
+    image_link = str(page.html.find('div.active img')[0].attrs.get('src'))
+    image_absolute_link = 'http://books.toscrape.com' + image_link.replace('../..', '')
+
+    # Use dictionary to write to the csv
+    data = {'Titre :': title, 'Catégorie:': category, 'Descripton :': production_descripton, 'UPC Code :': code_upc,
+             'Etoile :':star_rating, 'Prix hors taxe :': price_without_tax, 'Prix taxe compris :': price_with_tax,
+             'Stock :': inventory}
+
+    return (data,image_absolute_link)
 
 
-url = 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html '
-for urlss in urls_dune_category(url) :
-    print(urlss)
+url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
+session = HTMLSession()
+all_books_url = []
+data_all_books = {}
+links_all_images = []
+all_code_upc = []
 
+# get all the category links in the first page
+url = get_links_of_category_in_first_page(url)
 
-'''
-for url_une_page in url_des_page_category(url) :
-    print(url_une_page[0])
-    t = element_page(url_une_page[0])
+# get all the page links from each categories
+for single_url_of_category in url :
+    all_books_url = all_books_url + get_all_books_links_of_one_category(single_url_of_category)
 
-   #with open('chanpin.csv','a+',encoding='utf-8_sig',newline='') as f :
-    #   writer =  csv.writer(f)
-    #   writer.writerows(zip(t))
-'''
+# write all the informations asked
+with open('scape.csv','w',encoding='utf-8_sig',newline='') as file :
+    header = ['Titre :','Catégorie:','Descripton :','UPC Code :','Etoile :', 'Prix hors taxe :','Prix taxe compris :','Stock :']
+    writer =  csv.DictWriter(file,fieldnames=header)
+    writer.writeheader()
+    for single_url in all_books_url :
+        data,links_image = get_data_in_book_page(single_url)
+        code_upc = data['UPC Code :']
+        writer.writerow(data)
+        print(links_image)
+        print(code_upc)
+        image = session.get(links_image)
+        #make a image fold if there is not
+        if not os.path.exists('image') :
+            os.mkdir('image')
+        # name the images by using their upc code
+        with open('image\\'+ code_upc +'.jpg','wb') as file :
+            file.write(image.content)
