60df9310a767cc06f5ee977dbdcc113bd23e3628
Antoine Latrille <antoine.latrille@gmail.com>
2023-02-17 10:36:35 +0100

    Decoupage du script en fonctions par phases pour le livrable

--- scrapbookavecfonction.py
+++ scrapbookavecfonction.py
@@ -39,7 +39,7 @@ def get_links_of_category_in_first_page(first_page_url: str) -> List[str]:
     del category_links[0]
     del category_names[0]
 
-    return category_links,category_names
+    return category_links, category_names
 
 
 # get all the links in a single detail page of category
@@ -128,30 +128,18 @@ all_books_urls = []
 data_all_books = {}
 links_all_images = []
 all_code_upc = []
+if not os.path.exists('output_data'):
+    os.mkdir('output_data')
 
-# get all the category links in the first page
-print(f'Récupération des liens des catégories')
-category_urls,category_names = get_links_of_category_in_first_page(URL)
-print(f'{len(category_urls)} catégories trouvées')
-#make a CSV folder if there is not have
-if not os.path.exists('Fichier CSV'):
-    os.mkdir('Fichier CSV')
 
-# make a image folder if there is not have
-if not os.path.exists('image'):
-    os.mkdir('image')
-i = 0
-nombres_livres = 0
-# For every single category make a separate csv file named by their category name and write a header
-for single_url_of_category in category_urls:
-    all_books_urls_of_one_category = get_all_books_links_of_one_category(single_url_of_category)
-    # make a directory for all the files csv which is named by their category
-    csv_path = Path('Fichier CSV/' + category_names[i] + '.csv')
-    nombres_livres_dans_une_category = len(all_books_urls_of_one_category)
-    # get total number of the books
-    nombres_livres = nombres_livres_dans_une_category + nombres_livres
-    print(f'{nombres_livres_dans_une_category} livres trouvés dans la: {category_names[i]}')
-    print(f'Il y a {i+1} catégorie qu\'on a trouvé déjà')
+def run_phase1():
+    if not os.path.exists('output_data/phase1'):
+        os.mkdir('output_data/phase1')
+    product_url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+    print(f'Récupération des données du livre à l\'url: {product_url}')
+    data, _ = get_data_in_book_page(product_url)
+    csv_path = Path('output_data/phase1/' + data['UPC Code'] + '.csv')
+    del data['image link']
     with open(csv_path, 'w', encoding='utf-8_sig', newline='') as file:
         header = ['product_page_url',
                   'UPC Code',
@@ -161,20 +149,76 @@ for single_url_of_category in category_urls:
                   'Stock',
                   'Descripton',
                   'Catégorie',
-                  'Etoile',
-                  'image link']
+                  'Etoile']
         writer = csv.DictWriter(file, fieldnames=header)
         writer.writeheader()
-        i = i + 1
-        #For every single page in one category, get the informations neededs and download the pictures
-        for single_url_of_one_category in all_books_urls_of_one_category :
-            data, links_image = get_data_in_book_page(single_url_of_one_category)
-            writer.writerow(data)
-            image = session.get(links_image)
-            # name the images by using their upc code
-            # use pathlib to solve the problem of /
-            image_to_write = Path('image/' + data['UPC Code'] + '.jpg')
-            with open(image_to_write, 'wb') as file:
-                file.write(image.content)
-print(f'Il y a {nombres_livres} livres au total')
-print(f'Fini!')
+        writer.writerow(data)
+    print(f'Données écrites dans le fichier {csv_path}')
+
+
+def run_phase2():
+    if not os.path.exists('output_data/phase2'):
+        os.mkdir('output_data/phase2')
+    category_url = 'To do'
+    pass
+
+
+def run_phase3_4():
+    # get all the category links in the first page
+    print(f'Récupération des liens des catégories')
+    category_urls, category_names = get_links_of_category_in_first_page(URL)
+    print(f'{len(category_urls)} catégories trouvées')
+    # make a CSV folder if there is not have
+    if not os.path.exists('output_data'):
+        os.mkdir('output_data')
+
+    # make a image folder if there is not have
+    if not os.path.exists('output_data/images'):
+        os.mkdir('output_data/images')
+    i = 0
+    nombres_livres = 0
+    # For every single category make a separate csv file named by their category name and write a header
+    for single_url_of_category in category_urls:
+        all_books_urls_of_one_category = get_all_books_links_of_one_category(single_url_of_category)
+        # make a directory for all the files csv which is named by their category
+        csv_path = Path('output_data/' + category_names[i] + '.csv')
+        nombres_livres_dans_une_category = len(all_books_urls_of_one_category)
+        # get total number of the books
+        nombres_livres = nombres_livres_dans_une_category + nombres_livres
+        print(f'{nombres_livres_dans_une_category} livres trouvés dans la: {category_names[i]}')
+        print(f'Il y a {i + 1} catégorie qu\'on a trouvé déjà')
+        with open(csv_path, 'w', encoding='utf-8_sig', newline='') as file:
+            header = ['product_page_url',
+                      'UPC Code',
+                      'Titre',
+                      'Prix taxe compris',
+                      'Prix hors taxe',
+                      'Stock',
+                      'Descripton',
+                      'Catégorie',
+                      'Etoile',
+                      'image link']
+            writer = csv.DictWriter(file, fieldnames=header)
+            writer.writeheader()
+            i = i + 1
+            # For every single page in one category, get the informations neededs and download the pictures
+            for single_url_of_one_category in all_books_urls_of_one_category:
+                data, links_image = get_data_in_book_page(single_url_of_one_category)
+                writer.writerow(data)
+                image = session.get(links_image)
+                # name the images by using their upc code
+                # use pathlib to solve the problem of /
+                image_to_write = Path('output_data/images/' + data['UPC Code'] + '.jpg')
+                with open(image_to_write, 'wb') as file:
+                    file.write(image.content)
+    print(f'Il y a {nombres_livres} livres au total')
+    print(f'Fini!')
+
+
+print('Executé au chargement du module')
+
+if __name__ == '__main__':
+    print('Executé comme un script')
+    # run_phase1()
+    run_phase2()
+    run_phase3_4()
